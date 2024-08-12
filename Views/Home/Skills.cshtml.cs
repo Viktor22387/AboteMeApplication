5fc429a6cdb77449f09c3958ff2edb4388f5bf53
Harla_oq45@student.itstep.org <Harla_oq45@student.itstep.org>
2024-08-12 18:21:30 +0200

    Добавьте файлы проекта.

---
+++ Views/Home/Skills.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AboteMeApplication.Views.Home
+{
+    public class SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
